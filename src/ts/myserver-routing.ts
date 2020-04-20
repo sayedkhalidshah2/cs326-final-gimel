bd013518448a8a2513e73504569da5c3c1395611
Veronica <cpgusev@gmail.com>
2020-04-20 15:57:11 -0400

    added database functionality for adding resturaunts

--- src/ts/myserver-routing.ts
+++ src/ts/myserver-routing.ts
@@ -1,6 +1,7 @@
-let http = require('http');
-let url = require('url');
-let express = require('express');
+/* eslint-disable no-mixed-spaces-and-tabs */
+let http = require("http")
+let url = require("url")
+let express = require("express")
 
 export class MyServer {
 
@@ -12,168 +13,170 @@ export class MyServer {
     private router = express.Router();
 
     constructor(db) {
-	this.theDatabase = db;
-	// from https://enable-cors.org/server_expressjs.html
-	this.router.use((request, response, next) => {
-	    response.header('Content-Type','application/json');
-	    response.header('Access-Control-Allow-Origin', '*');
-	    response.header('Access-Control-Allow-Headers', '*');
-	    next();
-	});
-	// Serve static pages from a particular path.
-	this.server.use('/', express.static('./html'));
+    	this.theDatabase = db
+    	// from https://enable-cors.org/server_expressjs.html
+    	this.router.use((request, response, next) => {
+	    response.header("Content-Type","application/json")
+	    response.header("Access-Control-Allow-Origin", "*")
+	    response.header("Access-Control-Allow-Headers", "*")
+	    next()
+    	})
+    	// Serve static pages from a particular path.
+    	this.server.use("/", express.static("./html"))
 
-	this.server.use(express.json());
-	this.server.use(express.urlencoded({ extended: false }));
+    	this.server.use(express.json())
+    	this.server.use(express.urlencoded({ extended: false }))
 
-	this.server.use((req, res, next) => {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header(
-			"Access-Control-Allow-Headers",
-			"Origin, X-Requested-With, Content-Type, Accept"
-		);
-		next();
-	});
-	//// YOUR CODE GOES HERE
-	//// HANDLE CREATE, READ, UPDATE, AND DELETE OPERATIONS
-	this.router.get('/users/:userId/create', this.createHandler.bind(this));
-	this.router.get('/users/:userId/read', [this.errorHandler.bind(this), this.readHandler.bind(this) ]);
-	this.router.get('/users/:userId/update', [this.errorHandler.bind(this), this.updateHandler.bind(this) ]);
-	this.router.get('/users/:userId/delete', [this.errorHandler.bind(this), this.deleteHandler.bind(this) ]);
+    	this.server.use((req, res, next) => {
+    		res.header("Access-Control-Allow-Origin", "*")
+    		res.header(
+    			"Access-Control-Allow-Headers",
+    			"Origin, X-Requested-With, Content-Type, Accept"
+    		)
+    		next()
+    	})
+    	//// YOUR CODE GOES HERE
+    	//// HANDLE CREATE, READ, UPDATE, AND DELETE OPERATIONS
+    	// this.router.post("/menu", this.updateHandler.bind(this))
+    	this.router.post("/create", this.createHandler.bind(this))
+    	// this.router.get("/users/:userId/update", [this.errorHandler.bind(this), this.updateHandler.bind(this) ])
+    	// this.router.get("/users/:userId/delete", [this.errorHandler.bind(this), this.deleteHandler.bind(this) ])
 	
-	//// HANDLE ERRORS WITH A WILDCARD (*)
-	this.router.get('/*', this.errorHandler.bind(this));
+    	//// HANDLE ERRORS WITH A WILDCARD (*)
+    	// this.router.get("/*", this.errorHandler.bind(this))
 
-	// Start up the counter endpoint at '/counter'.
-	this.server.use('/counter', this.router);
+    	// Start up the counter endpoint at '/counter'.
+    	this.server.use("/", this.router)
     }
 
     private async errorHandler(request, response, next) : Promise<void> {
-	let value : boolean = await this.theDatabase.isFound(request.params['userId']+"-"+request.query.name);
-	if (!value) {
-	    response.write(JSON.stringify({'result' : 'error'}));
-	    response.end();
-	} else {
-	    next();
-	}
+    	let value : boolean = await this.theDatabase.isFound(request.params["userId"]+"-"+request.query.name)
+    	if (!value) {
+	    response.write(JSON.stringify({"result" : "error"}))
+	    response.end()
+    	} else {
+	    next()
+    	}
     }
     
     private async createHandler(request, response) : Promise<void> {
-		console.log('create')
-		await this.createCounter(request, response); //C
+    	console.log("adding resturaunt")
+    	await this.theDatabase.create(request.body)
+    	response.end()
     }
 
     private async readHandler(request, response): Promise<void> {
-		await this.readCounter(request.params['userId']+"-"+request.query.name, response);
-		console.log('read')
+    	await this.readCounter(request.params["userId"]+"-"+request.query.name, response)
+    	console.log("read")
     }
 
     private async updateHandler(request, response) : Promise<void> {
-		await this.updateCounter(request.params['userId']+"-"+request.query.name, request.query.value, response);
-		console.log('update')
+    	// await this.updateCounter(request.params["userId"]+"-"+request.query.name, request.query.value, response)
+    	console.log("update")
 
     }
 
     private async deleteHandler(request, response) : Promise<void> {
-		await this.deleteCounter(request.params['userId']+"-"+request.query.name, response);
-		console.log('delete')
+    	await this.deleteCounter(request.params["userId"]+"-"+request.query.name, response)
+    	console.log("delete")
 
     }
 
     public listen(port) : void  {
-	this.server.listen(port);
+    	this.server.listen(port)
     }
 
     public async createCounter(request, response) : Promise<void> {
-		const name = request.body['name'];
-		const cost = request.body['cost'];
-		const dscr = request.body['dscr'];
-		const grup = request.body['grup'];
-		const rstr = request.body['rstr'];
+    	console.log(request)
+    	// const name = request.body["name"]
+    	// const cost = request.body["cost"]
+    	// const dscr = request.body["dscr"]
+    	// const grup = request.body["grup"]
+    	// const rstr = request.body["rstr"]
 
-		console.log("Creating Menu Item With the Following Values:");
-		console.log("Item Name: "+request.body['name']);
-		console.log("Item Cost: "+request.body['cost']);
-		console.log("Item Description: "+request.body['dscr']);
-		console.log("Menu Group: "+request.body['grop']);
-		console.log("Restaurant: "+request.body['rstr']);
+    	// console.log("Creating Menu Item With the Following Values:")
+    	// console.log("Item Name: "+request.body["name"])
+    	// console.log("Item Cost: "+request.body["cost"])
+    	// console.log("Item Description: "+request.body["dscr"])
+    	// console.log("Menu Group: "+request.body["grop"])
+    	// console.log("Restaurant: "+request.body["rstr"])
 
-		await this.theDatabase.put(name,cost,dscr,grup,rstr);
-		response.write(JSON.stringify({
-			'result' : 'created',
-			'name' : name,
-			'cost' : cost,
-			'dscr' : dscr,
-			'grup' : grup,
-			'rstr' : rstr 
-		}));
-		response.end();
+    	// await this.theDatabase.put(name,cost,dscr,grup,rstr)
+    	// response.write(JSON.stringify({
+    	// 	"result" : "created",
+    	// 	"name" : name,
+    	// 	"cost" : cost,
+    	// 	"dscr" : dscr,
+    	// 	"grup" : grup,
+    	// 	"rstr" : rstr 
+    	// }))
+    	response.end()
     }
 
     public async errorCounter(name: string, response) : Promise<void> {
-	response.write(JSON.stringify({'result': 'error'}));
-	response.end();
+    	response.write(JSON.stringify({"result": "error"}))
+    	response.end()
     }
 
     public async readCounter(name: string, response) : Promise<void> {
-	let value = await this.theDatabase.get(name);
-	response.write(JSON.stringify({'result' : 'read',
-				       'name' : name,
-				       'value' : value }));
-	response.end();
+    	let value = await this.theDatabase.get(name)
+    	response.write(JSON.stringify({"result" : "read",
+				       "name" : name,
+				       "value" : value }))
+    	response.end()
     }
 
-	//Not Needed Because of Checking function in DB
+    //Not Needed Because of Checking function in DB
     // public async updateCounter(name: string, value: number, response) : Promise<void> {
-	// await this.theDatabase.put(name, value);
-	// response.write(JSON.stringify({'result' : 'updated',
-	// 			       'name' : name,
-	// 			       'value' : value }));
-	// response.end();
+    // await this.theDatabase.put(name, value);
+    // response.write(JSON.stringify({'result' : 'updated',
+    // 			       'name' : name,
+    // 			       'value' : value }));
+    // response.end();
     // }
     
     public async deleteCounter(name : string, response) : Promise<void> {
-	await this.theDatabase.del(name);
-	response.write(JSON.stringify({'result' : 'deleted',
-				       'value'  : name }));
-	response.end();
-	}
+    	await this.theDatabase.del(name)
+    	response.write(JSON.stringify({"result" : "deleted",
+				       "value"  : name }))
+    	response.end()
+    }
 
 
-// 	//Sample:
-// 	var express = require("express");
-// var app = express();
+	// 	//Sample:
+	// 	var express = require("express");
+	// var app = express();
 
-// // use `express.json()` and `express.urlencoded` to
-// // parse incoming requests with JSON payloads
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+	// // use `express.json()` and `express.urlencoded` to
+	// // parse incoming requests with JSON payloads
+	// app.use(express.json());
+	// app.use(express.urlencoded({ extended: false }));
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-// app.post("/hello", (req, res, next) => {
-//   // `req.body` is a native object since `express.json()`
-//   // and `express.urlencoded()` are taking care of the payload,
-//   // no need for `JSON.parse()` here
+	// app.use((req, res, next) => {
+	//   res.header("Access-Control-Allow-Origin", "*");
+	//   res.header(
+	//     "Access-Control-Allow-Headers",
+	//     "Origin, X-Requested-With, Content-Type, Accept"
+	//   );
+	//   next();
+	// });
+	// app.post("/hello", (req, res, next) => {
+	//   // `req.body` is a native object since `express.json()`
+	//   // and `express.urlencoded()` are taking care of the payload,
+	//   // no need for `JSON.parse()` here
 
-//   //If the entire object is wanted back
-//   const payload = {
-//     ...req.body,
-//   };
+	//   //If the entire object is wanted back
+	//   const payload = {
+	//     ...req.body,
+	//   };
 
-//   //If a singular part is wanted back
-//   //const payload = req.body["<attribute>"]
+	//   //If a singular part is wanted back
+	//   //const payload = req.body["<attribute>"]
 
-//   res.status(200).json(payload);
-// });
+	//   res.status(200).json(payload);
+	// });
 
-// app.listen(3000)
+	// app.listen(3000)
 	
 }
 
---
+++ src/ts/secure.ts
@@ -0,0 +1,1 @@
+export let URI: String = "mongodb+srv://Veronica:mayoryosh@bergercluster-fqlze.mongodb.net/test?retryWrites=true&w=majority"
