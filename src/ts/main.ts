d54083bd300ec941961435c398567ed0b7fcc5d9
Veronica <cpgusev@gmail.com>
2020-04-20 15:57:11 -0400

    added database functionality for adding resturaunts

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,6 @@
-$('#sideNav').on('hidden.bs.collapse', function () {
-    $("#sideNav").addClass("d-none d-sm-block collapse" )
+$("#sideNav").on("hidden.bs.collapse", function () {
+	$("#sideNav").addClass("d-none d-sm-block collapse" )
 })
-$('#sideNav').on('show.bs.collapse', function () {
-    $("#sideNav").removeClass("d-none d-sm-block")
+$("#sideNav").on("show.bs.collapse", function () {
+	$("#sideNav").removeClass("d-none d-sm-block")
 })
