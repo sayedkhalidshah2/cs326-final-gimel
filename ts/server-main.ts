58144fada8970ff122a62fe56137ef01e1633602
Veronica <cpgusev@gmail.com>
2020-04-21 23:26:59 -0400

    Delete server-main.ts

--- ts/server-main.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import { Database } from './mongo-database';
-import { MyServer } from './myserver-routing';
-
-const theDatabase = new Database('dergoose'); // CHANGE THIS
-const theServer = new MyServer(theDatabase);
-
-theServer.listen(8080);
