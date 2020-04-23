66cbbf3621c32a163a7753c829232ab1a785e794
Sayed Khalid Shah <sayedshah@Sayeds-MacBook-Pro-2.local>
2020-04-23 18:10:40 -0400

    CRUD operation seems to be woriking but object retuens null

--- html/clientSsb.js
+++ html/clientSsb.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-const url = "http://localhost:8080/counter"
+const url = "http://localhost:8080"
 
 async function postData(url, data) {
 	const resp = await fetch(url,
@@ -13,53 +13,53 @@ async function postData(url, data) {
 			},
 			redirect: "follow",
 			body: JSON.stringify(data)
-		})
-	return resp
+		});
+	return resp;
 }
 
 
 
 function addItem() {
 	(async () => {
-		let itemName = document.getElementById("itemname").value
-		let itemGroup = document.getElementById("itemgroup").value
-		let itemCost = document.getElementById("itemcost").value
-		let itemDescription = document.getElementById("itemdescription").value
-		const data = { "name" : itemName, "group":itemGroup
-			,"cost": itemCost, "desc": itemDescription } // -- (1)
-		const newURL = url + "/users/" + itemName + "/create" // used to be ?name=" + counterName; -- (2)
-		console.log("counterCreate: fetching " + newURL)
-		const resp = await postData(newURL, data) // used to be fetch -- (3)
-		const j = await resp.json()
+		let itemName = document.getElementById("itemname").value;
+		let itemCost = document.getElementById("itemcost").value;
+		let itemDescription = document.getElementById("itemdescription").value;
+		let itemGroup = document.getElementById("itemgroup").value;
+		const data = { "name" : itemName,"cost": itemCost, 
+						"desc": itemDescription, 'type':itemGroup }; // -- (1)
+		const newURL = url + "/menus/" + data +"/create";; // used to be ?name=" + counterName; -- (2)
+		console.log("AddingItemCreate: fetching " + newURL);
+		const resp = await postData(newURL, data) ;// used to be fetch -- (3)
+		const j = await resp.json();
 
 		if (j["result"] !== "error") {
-	    document.getElementById("output").innerHTML = "101: <b>" + userName + ", " + counterName + " created.</b>"
+	    document.getElementById("output").innerHTML = "101: <b>" + itemName + ", " + itemDescription + " created.</b>";
 		} else {
-	    document.getElementById("output").innerHTML = "100: " + userName + ", " + counterName + " not found.</b>"
+	    document.getElementById("output").innerHTML = "100: " + itemName + ", " + itemDescription + " not found.</b>";
 		}
-	})()
+	})();
 }
 
 
 
 function readItem() {
 	(async () => {
-		let itemName = document.getElementById("itemname").value
-		let itemGroup = document.getElementById("itemgroup").value
-		let itemCost = document.getElementById("itemcost").value
-		let itemDescription = document.getElementById("itemdescription").value
-		const data = { "name" : itemName, "group":itemGroup
-			,"cost": itemCost, "desc": itemDescription } // -- (1)
-		const newURL = url + "/users/" + itemName + "/read" // used to be ?name=" + counterName; -- (2)
-		console.log("counterRead: fetching " + newURL)
-		const resp = await postData(newURL, data)
-		const j = await resp.json()
+		let itemName = document.getElementById("itemname").value;
+		let itemCost = document.getElementById("itemcost").value;
+		let itemDescription = document.getElementById("itemdescription").value;
+		let itemGroup = document.getElementById("itemgroup").value;
+		const data = { "name" : itemName,"cost": itemCost, 
+						"desc": itemDescription, 'type':itemGroup }; // -- (1)
+		const newURL = url + "/menus/" + data + "/read"; // used to be ?name=" + counterName; -- (2)
+		console.log("counterRead: fetching " + newURL);
+		const resp = await postData(newURL, data);
+		const j = await resp.json();
 		if (j["result"] !== "error") {
-	    document.getElementById("output").innerHTML = "201: <b>"  + userName + ", " + counterName + " value = " + j["value"] + "</b>"
+	    document.getElementById("output").innerHTML = "201: <b>"  + itemName + ", " + itemDescription + " value = " + j["value"] + "</b>";
 		} else {
-	    document.getElementById("output").innerHTML = "200: " +  userName + ", " + counterName + " not found.</b>"
+	    document.getElementById("output").innerHTML = "200: " +  itemName + ", " + itemDescription + " not found.</b>";
 		}	    
-	})()
+	})();
 }
 
 
@@ -67,40 +67,40 @@ function readItem() {
 
 function updateItem() {
 	(async () => {
-		let itemName = document.getElementById("itemname").value
-		let itemGroup = document.getElementById("itemgroup").value
-		let itemCost = document.getElementById("itemcost").value
-		let itemDescription = document.getElementById("itemdescription").value
-		const data = { "name" : itemName, "group":itemGroup
-			,"cost": itemCost, "desc": itemDescription }
-		const newURL = url + "/users/" + itemName +"/update"  // used to be ?name=" + counterName; -- (2)
-		console.log("counterUpdate: fetching " + newURL)
-		const resp = await postData(newURL, data)    
-		const j = await resp.json()
+		let itemName = document.getElementById("itemname").value;
+		let itemCost = document.getElementById("itemcost").value;
+		let itemDescription = document.getElementById("itemdescription").value;
+		let itemGroup = document.getElementById("itemgroup").value;
+		const data = { "name" : itemName,"cost": itemCost, 
+						"desc": itemDescription, 'type':itemGroup };
+		const newURL = url + "/menus/" + data +"/update" ; // used to be ?name=" + counterName; -- (2)
+		console.log("counterUpdate: fetching " + newURL);
+		const resp = await postData(newURL, data);    
+		const j = await resp.json();
 		if (j["result"] !== "error") {
-	    document.getElementById("output").innerHTML = "301: <b>" + userName + ", " + counterName + " value = " + j["value"] + "</b>"
+	    document.getElementById("output").innerHTML = "301: <b>" + itemName + ", " + itemDescription + " value = " + j["value"] + "</b>";
 		} else {
-	    document.getElementById("output").innerHTML = "300: " + userName + ", " + counterName + " not found."
+	    document.getElementById("output").innerHTML = "300: " + itemName + ", " + itemDescription + " not found.";
 		}	    
-	})()
+	})();
 }
 
 function deleteItem() {
 	(async () => {
-		let itemName = document.getElementById("itemname").value
+		let itemName = document.getElementById("itemname").value;
 		// let itemGroup = document.getElementById("itemgroup").value;
 		// let itemCost = document.getElementById("itemcost").value;
 		// let itemDescription = document.getElementById("itemdescription").value;
-		const data = { "name" : itemName } // -- (1)
-		const newURL = url + "/users/" + itemName + "/delete" // used to be ?name=" + counterName; -- (2)
-		console.log("counterDelete: fetching " + newURL)
-		const resp = await postData(newURL, data)	    
-		const j = await resp.json()
+		const data = { "name" : itemName }; // -- (1)
+		const newURL = url + "/menus/" + data + "/delete"; // used to be ?name=" + counterName; -- (2)
+		console.log("counterDelete: fetching " + newURL);
+		const resp = await postData(newURL, data);	    
+		const j = await resp.json();
 		
 		if (j["result"] !== "error") {
-	    document.getElementById("output").innerHTML = "401: <b>" + userName + ", " + counterName + " deleted.</b>"
+	    document.getElementById("output").innerHTML = "401: <b>" + itemName + " deleted.</b>";
 		} else {
-	    document.getElementById("output").innerHTML = "400: " + userName + ", " + counterName + " not found.</b>"
+	    document.getElementById("output").innerHTML = "400: " + itemName + " not found.</b>";
 		}	    
-	})()
+	})();
 }
