6a923d94a02e5257f79387dd5c86d960ec9955ba
Joshua Read <joshread99@gmail.com>
2020-04-24 19:46:55 -0400

    Update

--- server/server-main.ts
+++ server/server-main.ts
@@ -4,4 +4,5 @@ import { MyServer } from "./myserver-routing"
 const theDatabase = new Database() 
 const theServer = new MyServer(theDatabase)
 
-theServer.listen(8080)
+var port = process.env.PORT || 8080
+theServer.listen(port)
