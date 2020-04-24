39aeb21a8549e6a5549d1c2abca156432775506d
Joshua Read <joshread99@gmail.com>
2020-04-24 19:46:55 -0400

    Update

--- server/server-main.js
+++ server/server-main.js
@@ -4,4 +4,5 @@ var mongo_database_1 = require("./mongo-database");
 var myserver_routing_1 = require("./myserver-routing");
 var theDatabase = new mongo_database_1.Database();
 var theServer = new myserver_routing_1.MyServer(theDatabase);
-theServer.listen(8080);
+var port = process.env.PORT || 8080;
+theServer.listen(port);
