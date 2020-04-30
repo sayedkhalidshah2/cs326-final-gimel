8d1f30613e68b6f41d3ef464de062e4622bba77e
Veronica <cpgusev@gmail.com>
2020-04-30 15:15:14 -0400

    attempting to generate pages dynamicaly

--- client/main.ts
+++ client/main.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 //TODO: disable tab when active
@@ -5,11 +6,18 @@
 //Nav bar controllers
 $("#sideNav").on("hidden.bs.collapse", function () {
 	$("#sideNav").addClass("d-none d-sm-block collapse" )
+=======
+//Nav bar controllers
+$("#sideNav").on("hidden.bs.collapse", function () {
+	$("#sideNav").addClass("d-none d-sm-block collapse" )
+	$("#sideNavBack").height("100vh")
+>>>>>>> 335da6e... attempting to generate pages dynamicaly
 })
 $("#sideNav").on("show.bs.collapse", function () {
 	$("#sideNavBack").height("auto")
 	$("#sideNav").removeClass("d-none d-sm-block")
 })
+<<<<<<< HEAD
 $("#icon").click( function() {
 	$(".nav-link").removeClass("active")
 })
@@ -19,6 +27,14 @@ $( document ).ready(async function() {
 	console.log("generating nav bar")
 	const resturaunts: Array<any> = await getData("/menus")
 	for(const resturaunt of resturaunts){
+=======
+
+$( document ).ready(async function() {
+	const resturaunts: Array<any> = await  getData("/menus")
+	console.log(resturaunts)
+	for(let resturaunt of resturaunts){
+		console.log(resturaunt)
+>>>>>>> 335da6e... attempting to generate pages dynamicaly
 		$("<a>",{
 			text: resturaunt.name,
 			title: resturaunt.name,
@@ -26,6 +42,7 @@ $( document ).ready(async function() {
 			id: "nav-" + resturaunt.name,
 			"data-toggle": "pill",
 			"role":"tab",
+<<<<<<< HEAD
 			"aria-controls": "pill-" + resturaunt.name,
 			href: "#pill-" + resturaunt.name,
 			"data-source": resturaunt.name,
@@ -185,6 +202,18 @@ async function login(code: string): Promise<any>{
 }
 
 async function getData(url): Promise<any>{
+=======
+			"aria-controls": "pill" + resturaunt.name,
+			"aria-selected": "false",
+			href: "#",
+
+		}).appendTo("#sideNavBack")
+	}
+})
+
+
+async function getData(url =  ""){
+>>>>>>> 335da6e... attempting to generate pages dynamicaly
 	const response = await fetch(url, {
 		method: "GET", // *GET, POST, PUT, DELETE, etc.
 		mode: "cors", // no-cors, *cors, same-origin
@@ -198,6 +227,7 @@ async function getData(url): Promise<any>{
 		// referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 		// body: JSON.stringify(data) // body data type must match "Content-Type" header
 	})
+<<<<<<< HEAD
 	return response // parses JSON response into native JavaScript objects
 }
 
@@ -224,6 +254,14 @@ async function deleteData(url): Promise<any>{
 async function postData(url, data): Promise<any> {
 	// Default options are marked with *
 	const response = await fetch(url, {
+=======
+	return response.json() // parses JSON response into native JavaScript objects
+}
+
+async function postData(url = "", data = {}) {
+	// Default options are marked with *
+	const response = await fetch(port + url, {
+>>>>>>> 335da6e... attempting to generate pages dynamicaly
 		method: "POST", // *GET, POST, PUT, DELETE, etc.
 		mode: "cors", // no-cors, *cors, same-origin
 		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -236,6 +274,7 @@ async function postData(url, data): Promise<any> {
 		referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
 		body: JSON.stringify(data) // body data type must match "Content-Type" header
 	})
+<<<<<<< HEAD
 	return response // parses JSON response into native JavaScript objects
 }
 
@@ -253,4 +292,7 @@ function getCookie(cname) {
 		}
 	}
 	return null
+=======
+	return response.json() // parses JSON response into native JavaScript objects
+>>>>>>> 335da6e... attempting to generate pages dynamicaly
 }
