c8d9c91a7cde64174f698d91e17d34c198b8420d
Veronica <cpgusev@gmail.com>
2020-05-01 22:23:46 -0400

    bulk update
    added:
    delete item
    deleting resturaunt
    adding resturaunt

--- client/main.ts
+++ client/main.ts
@@ -1,5 +1,8 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> d7ae7df... bulk update
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-use-before-define */
 //TODO: disable tab when active
@@ -7,6 +10,7 @@
 //Nav bar controllers
 $("#sideNav").on("hidden.bs.collapse", function () {
 	$("#sideNav").addClass("d-none d-sm-block collapse" )
+<<<<<<< HEAD
 =======
 =======
 //TODO: disable tab when active
@@ -17,6 +21,8 @@ $("#sideNav").on("hidden.bs.collapse", function () {
 	$("#sideNav").addClass("d-none d-sm-block collapse" )
 	$("#sideNavBack").height("100vh")
 >>>>>>> 335da6e... attempting to generate pages dynamicaly
+=======
+>>>>>>> d7ae7df... bulk update
 })
 $("#sideNav").on("show.bs.collapse", function () {
 	$("#sideNavBack").height("auto")
@@ -42,11 +48,14 @@ $("#icon").click( function() {
 
 // page refresh logic
 $( document ).ready(async function() {
+	console.log("generating nav bar")
 	const resturaunts: Array<any> = await getData("/menus")
-	console.log(resturaunts)
 	for(const resturaunt of resturaunts){
+<<<<<<< HEAD
 		console.log(resturaunt)
 >>>>>>> 335da6e... attempting to generate pages dynamicaly
+=======
+>>>>>>> d7ae7df... bulk update
 		$("<a>",{
 			text: resturaunt.name,
 			title: resturaunt.name,
@@ -222,10 +231,11 @@ async function getData(url): Promise<any>{
 =======
 			"aria-controls": "pill-" + resturaunt.name,
 			href: "#pill-" + resturaunt.name,
+			"data-source": resturaunt.name,
 			click: createPage,
 >>>>>>> aae94e7... added dynamic nav bar and adding/editing items
 
-		}).appendTo("#sideNavBack")
+		}).prependTo("#sideNavBack")
 
 		$("<div>",{
 			class: "tab-pane fade",
@@ -238,7 +248,7 @@ async function getData(url): Promise<any>{
 })
 
 //modal logic
-$("#exampleModal").on("show.bs.modal", function (event) {
+$("#exampleModal").on("show.bs.modal", function (event: any) {
 	const button = $(event.relatedTarget) // Button that triggered the modal
 	const rest = button.data("source") // Extract info from data-* attributes
 	// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
@@ -249,21 +259,38 @@ $("#exampleModal").on("show.bs.modal", function (event) {
 
 //add item logic
 $("#add-item-button").click( async function () {
-	const rest = $("#add-item-button").data("resturaunt")
-	const name = $("#add-item-name").val()
-	const cost = $("#add-item-cost").val()
-	const des = $("#add-item-des").val()
-	const cat = $("#add-item-cat").val()
+	console.log("add item clicked")
+	const rest: string = $("#add-item-button").data("resturaunt")
+	const name: string = $("#add-item-name").val() as string
+	const cost: number = $("#add-item-cost").val() as number
+	const des: string = $("#add-item-des").val() as string
+	const cat: string= $("#add-item-cat").val() as string
 
-	await addItem(rest,name, cost, des, cat)
+	await addItem(rest, name, cost, des, cat)
 	$("#add-item-form").trigger("reset")
+	await createPage(rest)
+})
+
+//add resturaunt logic
+$("#add-resturaunt-button").click( async function (event) {
+	console.log("add rest clicked")
+	const name = $("#add-rest-name").val() as string
+	const des = $("#add-rest-des").val() as string
+	await addRest(name,des)
+	location.reload()
+
 })
 
 
-
-async function createPage(click: any): Promise<any>{
-	const resturaunt = click.target.innerHTML
+async function createPage(resturaunt): Promise<any>{
 	// TODO: do this only once per load
+	if(resturaunt.target){
+		console.log("this is an event")
+		const related = $(resturaunt.target) // Button that triggered the modal
+		resturaunt = related.data("source") // Extract info from data-* attributes
+	}
+
+	console.log(`creating ${resturaunt}`)
 	const items: Array<any> = await getData("/menus/" + resturaunt)
 
 	let html = ""
@@ -276,6 +303,7 @@ async function createPage(click: any): Promise<any>{
 					<h5 class="card-title">${item.name}</h5>
 					<h6 class="card-subtitle mb-2 text-muted">$${item.cost}</h6>
 					<p class="card-text">${item.descr}</p>
+					<button type="button" class="btn btn-danger" onclick="deleteItem('${resturaunt}' , '${item.name}')">Remove item</button>
 				</div>
 			</div>`
 	}
@@ -289,12 +317,21 @@ async function createPage(click: any): Promise<any>{
 		"data-target": "#exampleModal"
 	})
 
+	const button2 =	$("<button>",{
+		class: "btn btn-danger",
+		text: "Delete this resturaunt",
+		"data-source": resturaunt,
+		click: deleteRest
+	})
 
-	$("#pill-" + resturaunt).html(html).append(button)
+
+
+	$("#pill-" + resturaunt).html(html).append(button, button2)
 	
 
 }
-async function addItem(rest,name, cost, des, cat): Promise<any>{
+async function addItem(rest = "default ",name="default", cost = 0, des= "default", cat = "default"): Promise<any>{
+	console.log(`adding item ${name}`)
 	const data = {
 		"name": name,
 		"cost": cost,
@@ -305,9 +342,39 @@ async function addItem(rest,name, cost, des, cat): Promise<any>{
 }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 async function getData(url =  ""){
 >>>>>>> 335da6e... attempting to generate pages dynamicaly
 =======
+=======
+async function addRest(rest="default",des="default"): Promise<any>{
+	console.log(`adding rest ${rest}`)
+	const data = {
+		"name": rest,
+		"descr": des,	}
+	await postData("/menus/", data)
+}
+
+async function deleteItem(resturant="default", item="default"): Promise<any>{
+	console.log(`deleting item ${name}`)
+	await deleteData(`/menus/${resturant}/${item}`)
+	await createPage(resturant)
+}
+
+async function deleteRest(restaurant): Promise<any>{
+	if(restaurant.target){
+		console.log("this is an event")
+		const related = $(restaurant.target) // Button that triggered the modal
+		restaurant = related.data("source") // Extract info from data-* attributes
+	}
+
+	console.log(`deleting rest ${restaurant}`)
+	await deleteData(`/menus/${restaurant}`)
+	location.reload()
+
+}
+
+>>>>>>> d7ae7df... bulk update
 async function getData(url): Promise<any>{
 >>>>>>> aae94e7... added dynamic nav bar and adding/editing items
 	const response = await fetch(url, {
@@ -352,6 +419,26 @@ async function postData(url, data): Promise<any> {
 	const response = await fetch(url, {
 =======
 	return response.json() // parses JSON response into native JavaScript objects
+}
+
+
+
+async function deleteData(url): Promise<any>{
+	const response = await fetch(url, {
+		method: "DELETE", // *GET, POST, PUT, DELETE, etc.
+		mode: "cors", // no-cors, *cors, same-origin
+		// cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+		// credentials: "same-origin", // include, *same-origin, omit
+		headers: {
+			"Content-Type": "application/json"
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		// redirect: "follow", // manual, *follow, error
+		// referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+		// body: JSON.stringify(data) // body data type must match "Content-Type" header
+	})
+	return response.json() // parses JSON response into native JavaScript objects
+
 }
 
 async function postData(url, data): Promise<any> {
